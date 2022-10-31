062130d3fa27b6a6d4ef2fd26b1f359d9ace0bfd
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-31 00:36:08 -0300

    Fazendo testes.

--- App/index.php
+++ App/index.php
@@ -8,4 +8,6 @@ include "config.php";
 
 include "rotas.php";
 
+// Link para consultar: https://www.devmedia.com.br/como-centralizar-divs-em-html-e-css/37568
+
 ?>
--- Database/registros.sql
+++ Database/registros.sql
@@ -1,9 +1,23 @@
+/*
+
+Métodos de criptografia de dados:
+
+	MD5("") -> permite apenas a criptograficação, não sendo possível, internamente, descriptografar.
+    
+    AES -> é composto por duas funções. Veja:
+    
+		AES_ENCRYPT("", "") -> função de criptograficação.
+        
+        AES_DECRYPT("", "") -> função de descriptograficação.
+        
+        O segundo parâmetro refere-se a uma chave de acesso.
+        
+Fonte: https://www.devmedia.com.br/introducao-a-criptografia-no-mysql/37179
+
+*/
+
 USE db_login;
 
-INSERT INTO usuario(email, senha) VALUES("teste1@gmail.com", "1234");
-
-INSERT INTO usuario(email, senha) VALUES("teste2@gmail.com", "4321");
-
-INSERT INTO usuario(email, senha) VALUES("teste3@gmail.com", "0000");
+INSERT INTO usuario(nome_usuario, email, senha) VALUES("Pablo", "teste1@gmail.com", MD5("211066"));
 
 SELECT * FROM usuario;
